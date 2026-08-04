35207b2d25b71db832b7f8cee31dfec5af0c2683
Amir Kenzo <100597400+AmirKenzo@users.noreply.github.com>
2026-08-04 15:12:40 +0330

    feat(security): persist crypto_key and webhook_secret in DB

--- app/utils/security/__init__.py
+++ app/utils/security/__init__.py
@@ -1,10 +1,12 @@
 """Security and crypto utilities."""
 
-from app.utils.security.crypto import CRYPTO_KEY, decrypt_data, encrypt_data, generate_key
+from app.utils.security.crypto import decrypt_data, encrypt_data, generate_key
+from app.utils.security.secrets_cache import get_crypto_key, get_webhook_secret
 
 __all__ = [
-    "CRYPTO_KEY",
     "decrypt_data",
     "encrypt_data",
     "generate_key",
+    "get_crypto_key",
+    "get_webhook_secret",
 ]
